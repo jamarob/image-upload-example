77207b6693a7bd039cf7b7ddc9994de45982267e
Jan Robert <janmartinrobert@gmail.com>
2021-06-09 11:20:14 +0200

    Revert "Add upload endpoint"
    This reverts commit 1fc4b20a3580c8887f85f7592915f39583c854c1.

--- backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
+++ backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
@@ -1,39 +1,28 @@
 package de.jamarob.photowall.controller;
 
 import de.jamarob.photowall.model.Photo;
-import de.jamarob.photowall.service.CloudinaryService;
 import de.jamarob.photowall.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("photo")
 public class PhotoController {
 
-    private final PhotoService photoService;
-    private final CloudinaryService cloudinaryService;
+    private final PhotoService service;
 
     @Autowired
-    public PhotoController(PhotoService photoService, CloudinaryService cloudinaryService) {
-        this.photoService = photoService;
-        this.cloudinaryService = cloudinaryService;
+    public PhotoController(PhotoService service) {
+        this.service = service;
     }
 
     @GetMapping
     public List<Photo> findAll(){
-        return photoService.findAll();
+        return service.findAll();
     }
 
-    @PostMapping("upload")
-    public Photo uploadImage(@RequestParam MultipartFile image) throws IOException {
-        File fileToUpload = File.createTempFile("photo", null);
-        image.transferTo(fileToUpload);
-        return cloudinaryService.uploadImage(fileToUpload);
-    }
 }
