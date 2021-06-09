ffeb65c9007376b24822383a45398ab261a74721
Jan Robert <janmartinrobert@gmail.com>
2021-06-09 15:21:08 +0200

    Remove unused imports

--- backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
+++ backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
@@ -9,9 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("photo")
