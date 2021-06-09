2b99c257b0553f7acfe23c5a313ba5a3afb255fb
Jan Robert <48794499+jamarob@users.noreply.github.com>
2021-06-09 12:21:39 +0200

    Add upload endpoint (#1)
    * Add upload to cloudinary
    * Save uploaded image to db

--- backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
+++ backend/src/main/java/de/jamarob/photowall/controller/PhotoController.java
@@ -1,28 +1,40 @@
 package de.jamarob.photowall.controller;
 
 import de.jamarob.photowall.model.Photo;
+import de.jamarob.photowall.service.CloudinaryService;
 import de.jamarob.photowall.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("photo")
 public class PhotoController {
 
-    private final PhotoService service;
+    private final PhotoService photoService;
+    private final CloudinaryService cloudinaryService;
 
     @Autowired
-    public PhotoController(PhotoService service) {
-        this.service = service;
+    public PhotoController(PhotoService photoService, CloudinaryService cloudinaryService) {
+        this.photoService = photoService;
+        this.cloudinaryService = cloudinaryService;
     }
 
     @GetMapping
     public List<Photo> findAll(){
-        return service.findAll();
+        return photoService.findAll();
     }
 
+    @PostMapping("upload")
+    public Photo uploadImage(@RequestParam MultipartFile image) throws IOException {
+        File fileToUpload = File.createTempFile("photo", null);
+        image.transferTo(fileToUpload);
+        Photo photoToSave = cloudinaryService.uploadImage(fileToUpload);
+        return photoService.savePhoto(photoToSave);
+    }
 }
